b7b6eb797587623f631830afda97567943890554
Ashley Sands <ashsands1@amstrike.com>
2020-03-02 19:14:48 +0000

    initial commit

---
+++ simpleSocket/Assets/Scripts/MoveAgent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAgent : MonoBehaviour
+{
+    NavMeshAgent myNavMeshAgent;
+    public bool followMouse = false;
+    Vector3 loc;
+    public float stopDist = 5f;
+    public float speed = 8;
+    public LayerMask layerMask;
+
+    void Start ()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update ()
+    {
+
+        print( Vector3.Distance( transform.position, loc ) + " < " + stopDist );
+
+        if ( followMouse || Input.GetMouseButtonDown( 0 ) )
+        {
+            SetDestinationToMousePosition();
+        }
+
+        if ( Vector3.Distance(transform.position, loc) < stopDist )
+        {
+            myNavMeshAgent.speed = 0;
+            transform.position = new Vector3(loc.x, transform.position.y, loc.z);
+        }
+        else
+        {
+            myNavMeshAgent.speed = speed;
+        }
+
+        transform.rotation = Quaternion.identity;
+
+
+    }
+
+    void SetDestinationToMousePosition ()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        if ( Physics.Raycast( ray, out hit, 300, layerMask ) ) 
+        {
+            myNavMeshAgent.SetDestination( hit.point );
+        }
+
+        loc = new Vector3((int)hit.point.x, hit.point.y, (int)hit.point.z );
+
+        Debug.DrawRay( ray.origin, ray.direction * 500, Color.red );
+
+    }
+}
---
+++ simpleSocket/Assets/Scripts/WebClient/Protocols/BaseProtocol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Protocol
+{
+
+    public abstract class BaseProtocol
+    {
+        public abstract char Idenity { get; }
+        public abstract string Name { get; }
+
+        private int chached_jsonLength = 0;
+
+        /// <summary>
+        /// Gets this class in json format :)
+        /// </summary>
+        /// <param name="jsonLength">OUT the length of the message</param>
+        /// <returns>this class as json</returns>
+        public string GetJson (out int jsonLength)
+        {
+            string jsonStr = JsonUtility.ToJson( this );
+            chached_jsonLength = jsonLength = jsonStr.Length;
+
+            return jsonStr;
+        }
+
+        /// <summary>
+        /// Gets the chached message length from the last time GetMessage was called
+        /// </summary>
+        /// <returns>chached message length</returns>
+        public int GetJsonLength ()
+        {
+            return chached_jsonLength;
+        }
+
+    }
+
+
+}
