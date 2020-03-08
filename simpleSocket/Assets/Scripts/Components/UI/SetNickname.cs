3472fd4ab54d5f0609551983a1645cdc152c19ab
Ashley Sands <ashsands1@amstrike.com>
2020-03-08 20:51:21 +0000

    added setnickname class

---
+++ simpleSocket/Assets/Scripts/Components/UI/SetNickname.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNickname : MonoBehaviour
+{
+    [SerializeField] private GameData gameData;
+    [SerializeField] private TMPro.TMP_InputField nicknameInput;
+
+    public void SetPlayerNickname ()
+    {
+        gameData.nickname = nicknameInput.text;
+    }
+}
