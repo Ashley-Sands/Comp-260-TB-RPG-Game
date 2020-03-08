9a4bd0632886a92961b69eaf91b34d70d17306cb
Ashley Sands <ashsands1@amstrike.com>
2020-03-08 01:22:34 +0000

    Added rand name and set on start in new ui scene

---
+++ simpleSocket/Assets/Scripts/SetTextFeildOnStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetTextFeildOnStart : MonoBehaviour
+{
+
+    [SerializeField] private TMP_InputField textInput;
+    [SerializeField] private RandomNames randomNames;
+
+    void Start()
+    {
+        
+        if ( textInput == null || randomNames == null)
+        {
+            Debug.LogError("Unable to set random name, either textinput or randomnames is missing");
+            return;
+        }
+
+        textInput.text = randomNames.GetRandomName() ;
+
+    }
+
+}
