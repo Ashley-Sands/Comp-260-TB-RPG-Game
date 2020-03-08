bb8d9ff47f4124358d57f17ecfc5f042fe8a6ddb
Ashley Sands <ashsands1@amstrike.com>
2020-03-08 18:53:16 +0000

    added methods for quit, set ui interactive and objects active via ui comps

---
+++ simpleSocket/Assets/Scripts/Components/SetGameObjectActive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameObjectActive : MonoBehaviour
+{
+    [SerializeField] private GameObject[] gameObj;
+
+    public void DisableUI ()
+    {
+        for ( int i = 0; i < gameObj.Length; i++ )
+            gameObj[ i ].SetActive( false );
+    }
+
+    public void EnableUI ()
+    {
+        for ( int i = 0; i < gameObj.Length; i++ )
+            gameObj[ i ].SetActive( true );
+    }
+}
---
+++ simpleSocket/Assets/Scripts/Components/UI/SetUIInteractable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SetUIInteractable : MonoBehaviour
+{
+
+    [SerializeField] private Selectable[] uiElements;
+
+    public void DisableUI()
+    {
+        for ( int i = 0; i < uiElements.Length; i++ )
+            uiElements[ i ].interactable = false;
+    }
+
+    public void EnableUI ()
+    {
+        for ( int i = 0; i < uiElements.Length; i++ )
+            uiElements[ i ].interactable = true;
+    }
+
+}
