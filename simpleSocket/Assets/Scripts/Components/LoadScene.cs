4bbe9a7f18a05337f712734378f952ee7fd63f85
Ashley Sands <ashsands1@amstrike.com>
2020-03-08 21:17:24 +0000

    added init scene and method to load scenes

---
+++ simpleSocket/Assets/Scripts/Components/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneToLoad;
+    [SerializeField] private bool loadOnStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if ( loadOnStart )
+            SceneManager.LoadScene( sceneToLoad, LoadSceneMode.Single );
+
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene( sceneToLoad, LoadSceneMode.Single );
+    }
+}
