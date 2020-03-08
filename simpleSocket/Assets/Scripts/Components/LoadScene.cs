cdafcc80ff5bd9a9345d8083540693ec175d6f1a
Ashley Sands <ashsands1@amstrike.com>
2020-03-08 21:53:10 +0000

    added method to start socket

--- simpleSocket/Assets/Scripts/Components/LoadScene.cs
+++ simpleSocket/Assets/Scripts/Components/LoadScene.cs
@@ -8,7 +8,6 @@ public class LoadScene : MonoBehaviour
     public string sceneToLoad;
     [SerializeField] private bool loadOnStart = false;
 
-    // Start is called before the first frame update
     void Start()
     {
 
---
+++ simpleSocket/Assets/Scripts/Components/UI/SetStartSocket.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartSocket : MonoBehaviour
+{
+    public void ConnectSocket()
+    {
+        SocketClient.ActiveSocket.running = true;
+    }
+}
