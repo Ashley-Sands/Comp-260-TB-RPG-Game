8c1159b28d6be1290f9139b25239fc998e5919cd
Ashley Sands <AS223343@falmouth.ac.uk>
2020-03-09 15:51:49 +0000

    added connection status

--- simpleSocket/Assets/Scripts/Components/GameData.cs
+++ simpleSocket/Assets/Scripts/Components/GameData.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ConnectionStatus { None, Connecting, Connected, Error }
+
+
 [CreateAssetMenu( fileName = "GameData", menuName = "GameData" )]
 public class GameData : ScriptableObject
 {
 
+    public delegate void connectionStatusChanged ( ConnectionStatus status );
+    public event connectionStatusChanged ConnectionStatusChanged;               // Make static ??
+
     public string nickname = "player";
 
+    private ConnectionStatus connStatus = ConnectionStatus.None;
+
+    public bool Connecting => connStatus == ConnectionStatus.Connecting;
+    public bool Connected => connStatus == ConnectionStatus.Connected;
+    public bool ConnectionError => connStatus == ConnectionStatus.Error;
+
+    
     private void OnEnable ()
     {
         Protocol.HandleProtocol.Inst.Bind( 'i', ReciveClientIdentityRequest );
@@ -23,6 +36,17 @@ public class GameData : ScriptableObject
 
         SocketClient.ActiveSocket.QueueMessage( clientIdentity as object );
 
+    }
+
+    public void SetStatus( ConnectionStatus status )
+    {
+        if (status != connStatus)
+        {
+
+            ConnectionStatusChanged.Invoke( status );
+            connStatus = status;
+
+        }
     }
 
     private void OnDisable ()
