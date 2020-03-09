a902a2fec31151049f764c3e4511bac9c3c4b2c5
Ashley Sands <AS223343@falmouth.ac.uk>
2020-03-09 16:44:15 +0000

    added set object on status change

---
+++ simpleSocket/Assets/Scripts/Components/SetActiveOnConnectionStatusChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveOnConnectionStatusChange : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+    [SerializeField] private ConnectionStatus activeStatus = ConnectionStatus.Connected;
+
+    [SerializeField] private GameData gameData;
+
+    private void Awake ()
+    {
+
+        gameData.ConnectionStatusChanged += ConnectionStatusChanged;
+
+    }
+
+    private void ConnectionStatusChanged ( ConnectionStatus status )
+    {
+        if ( status == activeStatus )
+            obj.SetActive( true );
+        else
+            obj.SetActive( false );
+
+
+    }
+
+    private void OnDestroy ()
+    {
+        gameData.ConnectionStatusChanged -= ConnectionStatusChanged;
+    }
+}
