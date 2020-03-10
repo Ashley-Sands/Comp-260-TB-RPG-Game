b1b3e14e8ed91d95f577f38e01da861720602889
Ashley Sands <ashsands1@amstrike.com>
2020-03-10 22:19:12 +0000

    added set lobby data

---
+++ simpleSocket/Assets/Scripts/Components/UI/SetLobbyData.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetLobbyData : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI gameName; 
+    [SerializeField] private TextMeshProUGUI clients;
+    [SerializeField] private TextMeshProUGUI details;
+    [SerializeField] private float startsAt = 0;
+
+    bool countdownIsRunning;
+
+    private void Awake ()
+    {
+        Protocol.HandleProtocol.Inst.Bind( 'd', ReceiveGameInfo );
+
+        ReceiveGameInfo( null );
+
+    }
+
+    private void ReceiveGameInfo ( Protocol.BaseProtocol protocol)
+    {
+
+        startsAt = SocketClient.ActiveGameData.gameStartsAt;
+
+        if ( !countdownIsRunning )
+            StartCoroutine( Countdown() );
+
+        UpdateUI();
+
+    }
+
+    private IEnumerator Countdown()
+    {
+
+        countdownIsRunning = true;
+
+        float remainTime = startsAt - Time.time;
+
+        while ( remainTime > 0f )
+        {
+            yield return new WaitForSeconds( 1f );
+            remainTime = Mathf.Max(0, startsAt - Time.time);
+
+            details.SetText( string.Format( "Match Starts In {0} secs", Helpers.GetTime( Mathf.FloorToInt(remainTime) ) ) );
+
+        }
+
+        countdownIsRunning = false;
+
+    }
+
+    private void UpdateUI()
+    {
+        GameData gameData = SocketClient.ActiveGameData;
+
+        gameName.SetText( gameData.gameName );
+        clients.SetText( string.Join( "\n", gameData.currentGamePlayers ) );
+
+    }
+
+    private void OnDestroy ()
+    {
+        Protocol.HandleProtocol.Inst.Unbind( 'd', ReceiveGameInfo );
+    }
+
+
+}
