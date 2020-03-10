bff3dab1f34507d3469ba62ee0e4f156f6461676
Ashley Sands <ashsands1@amstrike.com>
2020-03-10 22:18:57 +0000

    added helper to get time as formated string

---
+++ simpleSocket/Assets/Scripts/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helpers 
+{
+
+	public static string GetTime( int seconds )
+	{
+		System.TimeSpan time = new System.TimeSpan(0, 0, seconds);
+
+		return time.ToString();// string.Format(  )
+
+	}
+
+}
