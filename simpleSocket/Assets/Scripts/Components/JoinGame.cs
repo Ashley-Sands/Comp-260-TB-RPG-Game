d6eb2d46ff238905db6a3cb1566498d22b6f3a17
Ashley Sands <ashsands1@amstrike.com>
2020-03-10 16:47:30 +0000

    started to add join game

---
+++ simpleSocket/Assets/Scripts/Components/JoinGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Protocol;
+
+public class JoinServer : MonoBehaviour
+{
+
+    private void Awake ()
+    {
+        HandleProtocol.Inst.Bind( 's', JoinGame );
+    }
+
+    void JoinGame( BaseProtocol protocol )
+    {
+
+        StatusProtocol status = protocol as StatusProtocol;
+
+        if (status.IsType(StatusProtocol.Type.Game) && status.ok)
+        {
+            // 
+        }
+
+    }
+
+}
