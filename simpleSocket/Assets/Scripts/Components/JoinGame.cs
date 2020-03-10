05692baaa71bef355c628dd8338c14cc1afb9128
Ashley Sands <ashsands1@amstrike.com>
2020-03-10 20:26:16 +0000

    added change scene when joining game

--- simpleSocket/Assets/Scripts/Components/JoinGame.cs
+++ simpleSocket/Assets/Scripts/Components/JoinGame.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Protocol;
 
-public class JoinServer : MonoBehaviour
+public class JoinGame : MonoBehaviour
 {
 
     private void Awake ()
     {
-        HandleProtocol.Inst.Bind( 's', JoinGame );
+        HandleProtocol.Inst.Bind( 's', Join_Game );
     }
 
-    void JoinGame( BaseProtocol protocol )
+    void Join_Game( BaseProtocol protocol )
     {
 
         StatusProtocol status = protocol as StatusProtocol;
 
-        if (status.IsType(StatusProtocol.Type.Game) && status.ok)
+        if ( status.IsType(StatusProtocol.Type.Game) && status.ok)
         {
-            // 
+            SceneManager.LoadScene( "GameLobby", LoadSceneMode.Single );
+            SocketClient.ActiveGameData.SetGameStatus( GameData.GameStatus.Active ); 
         }
+
+    }
+
+    private void OnDestroy ()
+    {
+        HandleProtocol.Inst.Unbind( 's', Join_Game );
 
     }
 
