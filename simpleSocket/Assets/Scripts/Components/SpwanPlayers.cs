2c0fefdf1f25f6fa0868b11bad476f0412dca728
Ashley Sands <ashsands1@amstrike.com>
2020-03-11 22:59:00 +0000

    fixed spwaning player in wrong position

--- simpleSocket/Assets/Scripts/Components/SpwanPlayers.cs
+++ simpleSocket/Assets/Scripts/Components/SpwanPlayers.cs
@@ -7,11 +7,11 @@ public class SpwanPlayers : MonoBehaviour
 	[SerializeField] private Transform[] spwanPoints;
 	[SerializeField] private MoveAgent playerPrefab;
 
-	private void Awake ()
+	private void Start ()
 	{
 
 		// spwan the local player
-		SpwanPlayer( SocketClient.ActiveGameData.currentPlayerID );
+		SpwanPlayer( SocketClient.ActiveGameData.playerID );
 
 		// bind onto player joined game.
 
@@ -20,6 +20,9 @@ public class SpwanPlayers : MonoBehaviour
 
 	void SpwanPlayer( int playerId )
 	{
+
+		Protocol.MessageProtocol message = new Protocol.MessageProtocol( "Spwaning @ " + playerId ) { from_client = GameData.GAME_CLIENT_NAME };
+		SocketClient.ActiveSocket.QueueLocalMessage( message );
 
 		MoveAgent player = Instantiate<MoveAgent>( playerPrefab, spwanPoints[ playerId ].position, Quaternion.identity );
 		player.PlayerId = playerId;
