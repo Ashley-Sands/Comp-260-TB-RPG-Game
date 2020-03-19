8bf0bb9f6dd9a428b744f6875e82826c3eabfb8a
Ashley Sands <ashsands1@amstrike.com>
2020-03-19 01:14:08 +0000

    added setNickname from Game data :)

---
+++ simpleSocket/Assets/Scripts/Components/UI/SetNickname_fromGameData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNickname_fromGameData : MonoBehaviour
+{
+
+    [SerializeField] private TMPro.TextMeshProUGUI text;
+
+    private void Start ()
+    {
+        if ( text == null )
+        {
+            Debug.LogError( "Unable to set random name, text is missing" );
+            return;
+        }
+
+        text.SetText( SocketClient.ActiveGameData.CurrentPlayerName );
+    }
+}
